006862e3899f74b589339133c816a8585363e0d2
AlexAhn <144298841+Alex-SH-Ahn@users.noreply.github.com>
2024-04-08 16:53:54 +0900

    [Bronze V] Title: 오늘 날짜, Time: 196 ms, Memory: 17104 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,11 @@
+import java.time.*;
+
+// 2024.04.08 16:36
+
+class Main {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+
+        System.out.println(now);
+    }
+}
