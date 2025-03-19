ed6f5ca57dac7b4a952b651baeeb77870c81c612
AlexAhn <144298841+Alex-SH-Ahn@users.noreply.github.com>
2025-03-19 12:29:54 +0900

    [Bronze III] Title: 최소, 최대, Time: 424 ms, Memory: 86324 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -0,0 +1,21 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => el.split(" ").map(Number));
+
+let x = input[0];
+let arr = input[1];
+let min = arr[0]
+let max = arr[0]
+for (let i = 1; i < arr.length; i++) {
+  if (arr[i] > max) {
+    max = arr[i];
+  }
+  if (arr[i] < min) {
+    min = arr[i];
+  }
+}
+// console.log(x, arr)
+console.log(min, max);
