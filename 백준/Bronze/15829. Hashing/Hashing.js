453a596970013cd6ecdb53007370411337b93df2
AlexAhn <144298841+Alex-SH-Ahn@users.noreply.github.com>
2025-03-25 09:39:45 +0900

    [Bronze II] Title: Hashing, Time: 100 ms, Memory: 9588 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -7,11 +7,15 @@ const input = require('fs')
 const L = Number(input[0]);
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 const text = input[1];
+
+const MOD = 1234567891;
 let sum = 0;
+let power = 1;
 
 for (let i = 0; i < L; i++) {
   let index = alphabet.indexOf(text[i]);
-  sum += (index+1) * Math.pow(31, i);
+  sum = (sum + ((index + 1) * power) % MOD) % MOD;
+  power = (power * 31) % MOD; // 31^i 값을 누적하여 계산
 }
 
 console.log(sum);
